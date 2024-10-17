440a301f1bd291cedaf446c30e9a214054a99986
mozta <perezaguirrerafael@gmail.com>
2024-10-16 18:15:43 -0600

    my first change on tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="min-h-screen bg-orange-300 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-center mb-4">
+          Dragon Ball Z
+        </h1>
+      </div>
     </>
   )
 }
