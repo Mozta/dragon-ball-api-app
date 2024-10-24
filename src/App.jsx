33ee4731f03990ce5abd16ea557d52f64078341f
mozta <perezaguirrerafael@gmail.com>
2024-10-23 18:31:59 -0600

    add gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { Gallery } from './components/Gallery'
 
 function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-orange-300 flex items-center justify-center">
+      <div className="min-h-screen bg-orange-300 flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold text-center mb-4">
           Dragon Ball Z
         </h1>
+        <Gallery />
       </div>
     </>
   )
